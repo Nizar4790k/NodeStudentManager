370382d305f8522482b6e0cdebe1d6b2151fd3d3
Nizar4790k <nizar4790k@gmail.com>
2022-12-21 16:42:30 -0400

    se agregaron los componentes de login y registro respectivamente

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Components/Login/Login';
+import Register from './Components/Register/Register';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState({});
+
+  const onLoadUser = (user) => {
+    sessionStorage.setItem('user', JSON.stringify(user));
+    setUser(user);
+  };
+
+  const onSignOut = () => {
+    sessionStorage.clear();
+    setUser({});
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login onLoadUser={onLoadUser} />}></Route>
+          <Route
+            path="/Login"
+            element={<Login onLoadUser={onLoadUser} />}
+          ></Route>
+          <Route path="/Register" element={<Register />} />
+           
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import "./Login.css";
+
+
+const Login = ({ onLoadUser }) => {
+
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+
+
+        const user = JSON.parse(sessionStorage.getItem('user'));
+
+        if (user) {
+            navigate('/ProductList')
+        }
+
+    }, [])
+
+    const authenticate = () => {
+
+
+
+        if (!email || !password) {
+            alert("The credentials are wrong");
+            return;
+        }
+
+        fetch(process.env.REACT_APP_SERVER + '/login', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        })
+            .then(response => response.json())
+            .then(result => {
+
+
+                switch (result.status) {
+                    case "ACCESS_DENIED":
+                        alert("The credentials are wrong");
+                        break;
+                    case "ACCESS_GRANTED":
+
+                        sessionStorage.setItem('user', JSON.stringify(result.user));
+                        onLoadUser(result.user);
+                        navigate("/ProductList");
+
+                        break;
+                    default:
+                        alert("ERROR IN THE SERVER");
+                        break;
+                }
+            }).catch(err => console.log(err));
+
+
+
+    }
+
+    const onEmailChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const onPasswordChange = (e) => {
+        setPassword(e.target.value)
+    }
+
+    return (
+        <div className="wrapper">
+
+            <div className="form-signin" >
+                <h1 className="form-signin-heading">Bienvenidos a Node Student Manager!</h1>
+                <h4 >Por favor, inicie sesión</h4>
+                <input type="text" id="email" className="form-control" name="email" onChange={onEmailChange} placeholder="Email" autoFocus="" />
+                <br></br>
+
+                <input type="password" id="password" className="form-control" onChange={onPasswordChange} name="password" placeholder="Contraseña" />
+
+                <br></br>
+                <button className="btn btn-lg btn-primary btn-block" onClick={() => { authenticate() }}>Iniciar Sesión</button>
+
+                <br></br>
+                <br></br>
+
+                <button type="button" className="btn btn-link" onClick={() => { navigate("/Register") }}>Registrarse</button>
+            </div>
+
+
+        </div>
+
+
+    );
+
+
+
+};
+
+
+
+export default Login;
